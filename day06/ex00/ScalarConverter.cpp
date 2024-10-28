c276d43cc5ac88fefda99fbe5a853371740723ca
Mohammed Sitni <msitni1337@gmail.com>
2024-10-28 13:11:24 +0100

    Converter

--- day06/ex00/ScalarConverter.cpp
+++ day06/ex00/ScalarConverter.cpp
@@ -15,6 +15,17 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &sc)
 ScalarConverter::~ScalarConverter()
 {
 }
+/*
+static bool isNumeric(std::string s)
+{
+	size_t i = 0;
+	for (; i < s.length() && s[i] >= '0' && s[i] <= '9'; i++)
+	{}
+	if (i && i == s.length())
+		return true;
+	return false;
+}
+*/
 static void ConvertChar(std::string s)
 {
 	if (std::isprint(s[0]) && std::isdigit(s[0]) == 0)
@@ -32,15 +43,41 @@ static void ConvertChar(std::string s)
 		std::cout << "double: " << std::fixed << std::setprecision(1) << atof(s.c_str()) << '\n';
 	}
 }
+static void ConvertInt(std::string s)
+{
+	long long value = atol(s.c_str());
+	std::cout << value << '\n';
+	if (value <= INT_MAX && value >= INT_MIN)
+	{
+		if (value >= ' ' && value <= '~')
+			std::cout << "char: " << static_cast<char>(value) << '\n';
+		else if (value <= 127)
+			std::cout << "char: Non displayable\n";
+		else
+			std::cout << "char: impossible\n";
+		std::cout << "int: " << static_cast<int>(value) << '\n';
+	}
+	else
+	{
+		std::cout << "char: impossible\n";
+		std::cout << "int: " << "Overflow" << '\n';
+	}
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(value) << "f\n";
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(value) << '\n';
+}
 void ScalarConverter::convert(std::string s)
 {
 	if (s.empty())
 	{
 		std::cout << "Empty String Provided.\n";
-		return;
 	}
-	if (s.length() == 1)
+	else if (s.length() == 1)
 	{
-		return ConvertChar(s);
+		ConvertChar(s);
+	}
+	else //if (isNumeric(s))
+	else
+	{
+		ConvertInt(s);
 	}
 }
--- day06/ex00/ScalarConverter.hpp
+++ day06/ex00/ScalarConverter.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <climits>
 #include <cstdlib>
 
 class ScalarConverter
