b9ebfdd887543cb1ca63367c744b4fb711951256
Mohammed Sitni <msitni1337@gmail.com>
2024-11-13 14:08:57 +0100

    MAC CLUSTER:  day09 - ex00 some progress done

--- day09/ex00/BitcoinExchange.cpp
+++ day09/ex00/BitcoinExchange.cpp
@@ -1,23 +1,40 @@
 #include "BitcoinExchange.hpp"
 
-BitcoinExchange::BitcoinExchange(std::ifstream& db_file)
+static inline unsigned long get_date_key(std::string date)
+{
+    unsigned long date_key;
+    char *str;
+
+    date_key = std::strtoul(date.c_str(), &str, 10) * 100;
+    date_key += std::strtoul(str + 1, &str, 10) * 10;
+    date_key += std::strtoul(str + 1, &str, 10);
+
+    return date_key;
+}
+BitcoinExchange::BitcoinExchange(std::ifstream &db_file)
 {
     std::string line;
+    getline(db_file, line);
     while (db_file.eof() == false)
     {
         getline(db_file, line);
-        std::cout << line << '\n';
+        std::string date;
+        std::string rate;
+        size_t comma = line.find(',');
+        if (comma == std::string::npos)
+            continue;
+        date = line.substr(0, comma);
+        rate = line.substr(comma + 1);
+        _db[get_date_key(date)] = std::strtof(rate.c_str(), NULL);
     }
 }
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &be)
-{
-    (void) be;
-}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &be) : _db(be._db)
+{}
 BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &be)
 {
-    (void) be;
+    if (this != &be)
+        _db = be._db;
     return *this;
 }
 BitcoinExchange::~BitcoinExchange()
-{
-}
+{}
--- day09/ex00/BitcoinExchange.hpp
+++ day09/ex00/BitcoinExchange.hpp
@@ -3,14 +3,16 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <cstdlib>
 
 class BitcoinExchange
 {
 private:
-    std::map<unsigned long, unsigned int> db;
+    std::map<unsigned long, float> _db;
+
 public:
-    BitcoinExchange(std::ifstream& db_file);
-    BitcoinExchange(const BitcoinExchange& be);
-    BitcoinExchange& operator=(const BitcoinExchange& be);
+    BitcoinExchange(std::ifstream &db_file);
+    BitcoinExchange(const BitcoinExchange &be);
+    BitcoinExchange &operator=(const BitcoinExchange &be);
     ~BitcoinExchange();
 };
